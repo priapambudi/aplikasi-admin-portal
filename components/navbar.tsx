ec7c0a4c72d3a5a03b71a59df5a2a622c9c14555
priapambudi <priapambudi97@gmail.com>
2024-09-24 13:11:57 +0700

    feat: add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,30 @@
-const Navbar = () => {
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switcher";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
+
+const Navbar = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await db.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
-    <div>
-      <h1>ini navbar</h1>
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton />
+        </div>
+      </div>
     </div>
   );
 };
